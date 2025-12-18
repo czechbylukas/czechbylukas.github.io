d9f44086940a894afef9dfedce2adb1cc110d757
czechbylukas <ucitellukas@gmail.com>
2025-12-18 02:53:35 +0100

    Update 13_perfective_vs_imperfective.js

--- data/13_perfective_vs_imperfective.js
+++ data/13_perfective_vs_imperfective.js
@@ -1,90 +1,269 @@
-// data/1_usefulphrases.js
 const vocab = [
   // Questions about verb aspect
   {
-    czech: "ukončený (jednou)",
-    english: "Question: a verb that is PERFECTIVE shows that the action is:",
-    spanish: "Pregunta: un verbo PERFECTIVO muestra que la acción es:",
-    german: "Frage: Ein perfektives Verb zeigt an, dass die Handlung ist:"
+    czech:   "ukončený",
+    english: "A verb that is PERFECTIVE means that the action is ______",
+    spanish: "Un verbo PERFECTIVO muestra que la acción es ______",
+    german:  "Ein perfektives Verb zeigt an, dass die Handlung ist ______"
   },
   {
-    czech: "neukončený (opakovaný)",
-    english: "Question: a verb that is IMperfective shows that the action is:",
-    spanish: "Preguta: un verbo IMperfectivo muestra que la acción es:",
-    german: "Frage: Ein imperfektives Verb zeigt an, dass die Handlung ist:"
+    czech:   "neukončený",
+    english: "A verb that is IMPERFECTIVE shows that the action is ______",
+    spanish: "Un verbo IMPERFECTIVO muestra que la acción es ______",
+    german:  "Ein imperfektives Verb zeigt an, dass die Handlung ist ______"
   },
 
-
   // Perfective / Imperfective verb pairs
-  { czech: "přečíst (perf.)", english: "to read (perf.)", spanish: "přečíst (perf.)", german: "lesen (perf.)" },
-  { czech: "číst (imperf.)", english: "to read (imperf.)", spanish: "číst (imperf.)", german: "lesen (imperf.)" },
-
-  { czech: "udělat (perf.)", english: "to do / make (perf.)", spanish: "udělat (perf.)", german: "machen (perf.)" },
-  { czech: "dělat (imperf.)", english: "to do / make (imperf.)", spanish: "dělat (imperf.)", german: "machen (imperf.)" },
-
-  { czech: "dát (perf.)", english: "to give (perf.)", spanish: "dát (perf.)", german: "geben (perf.)" },
-  { czech: "dávat (imperf.)", english: "to give (imperf.)", spanish: "dávat (imperf.)", german: "geben (imperf.)" },
-
-  { czech: "podívat se (perf.)", english: "to look (perf.)", spanish: "podívat se (perf.)", german: "ansehen (perf.)" },
-  { czech: "dívat se (imperf.)", english: "to look (imperf.)", spanish: "dívat se (imperf.)", german: "sehen (imperf.)" },
-
-  { czech: "dostat (perf.)", english: "to get (perf.)", spanish: "dostat (perf.)", german: "bekommen (perf.)" },
-  { czech: "dostávat (imperf.)", english: "to get (imperf.)", spanish: "dostávat (imperf.)", german: "bekommen (imperf.)" },
-
-  { czech: "sníst (perf.)", english: "to eat up (perf.)", spanish: "sníst (perf.)", german: "aufessen (perf.)" },
-  { czech: "jíst (imperf.)", english: "to eat (imperf.)", spanish: "jíst (imperf.)", german: "essen (imperf.)" },
-
-  { czech: "koupit (perf.)", english: "to buy (perf.)", spanish: "koupit (perf.)", german: "kaufen (perf.)" },
-  { czech: "kupovat (imperf.)", english: "to buy (imperf.)", spanish: "kupovat (imperf.)", german: "kaufen (imperf.)" },
-
-  { czech: "vyluxovat (perf.)", english: "to vacuum (perf.)", spanish: "vyluxovat (perf.)", german: "staubsaugen (perf.)" },
-  { czech: "luxovat (imperf.)", english: "to vacuum (imperf.)", spanish: "luxovat (imperf.)", german: "staubsaugen (imperf.)" },
-
-  { czech: "namalovat obraz (perf.)", english: "to paint a picture (perf.)", spanish: "namalovat obraz (perf.)", german: "ein Bild malen (perf.)" },
-  { czech: "malovat obraz (imperf.)", english: "to paint a picture (imperf.)", spanish: "malovat obraz (imperf.)", german: "ein Bild malen (imperf.)" },
-
-  { czech: "umýt (perf.)", english: "to wash (perf.)", spanish: "umýt (perf.)", german: "waschen (perf.)" },
-  { czech: "mýt (imperf.)", english: "to wash (imperf.)", spanish: "mýt (imperf.)", german: "waschen (imperf.)" },
-
-  { czech: "opravit (perf.)", english: "to repair (perf.)", spanish: "opravit (perf.)", german: "reparieren (perf.)" },
-  { czech: "opravovat (imperf.)", english: "to repair (imperf.)", spanish: "opravovat (imperf.)", german: "reparieren (imperf.)" },
-
-  { czech: "vyprat (perf.)", english: "to wash clothes (perf.)", spanish: "vyprat (perf.)", german: "waschen (perf.)" },
-  { czech: "prát (imperf.)", english: "to wash clothes (imperf.)", spanish: "prát (imperf.)", german: "waschen (imperf.)" },
-
-  { czech: "vypít (perf.)", english: "to drink up (perf.)", spanish: "vypít (perf.)", german: "austrinken (perf.)" },
-  { czech: "pít (imperf.)", english: "to drink (imperf.)", spanish: "pít (imperf.)", german: "trinken (imperf.)" },
-
-  { czech: "zaplatit (perf.)", english: "to pay (perf.)", spanish: "zaplatit (perf.)", german: "bezahlen (perf.)" },
-  { czech: "platit (imperf.)", english: "to pay (imperf.)", spanish: "platit (imperf.)", german: "bezahlen (imperf.)" },
-
-  { czech: "prodat (perf.)", english: "to sell (perf.)", spanish: "prodat (perf.)", german: "verkaufen (perf.)" },
-  { czech: "prodávat (imperf.)", english: "to sell (imperf.)", spanish: "prodávat (imperf.)", german: "verkaufen (imperf.)" },
-
-  { czech: "připravit (perf.)", english: "to prepare (perf.)", spanish: "připravit (perf.)", german: "vorbereiten (perf.)" },
-  { czech: "připravovat (imperf.)", english: "to prepare (imperf.)", spanish: "připravovat (imperf.)", german: "vorbereiten (imperf.)" },
-
-  { czech: "napsat (perf.)", english: "to write (perf.)", spanish: "napsat (perf.)", german: "schreiben (perf.)" },
-  { czech: "psát (imperf.)", english: "to write (imperf.)", spanish: "psát (imperf.)", german: "schreiben (imperf.)" },
-
-  { czech: "zatelefonovat (perf.)", english: "to make a call (perf.)", spanish: "zatelefonovat (perf.)", german: "anrufen (perf.)" },
-  { czech: "telefonovat (imperf.)", english: "to make a call (imperf.)", spanish: "telefonovat (imperf.)", german: "telefonieren (imperf.)" },
-
-  { czech: "naučit se (perf.)", english: "to learn (perf.)", spanish: "naučit se (perf.)", german: "lernen (perf.)" },
-  { czech: "učit se (imperf.)", english: "to learn (imperf.)", spanish: "učit se (imperf.)", german: "lernen (imperf.)" },
-
-  { czech: "uklidit (perf.)", english: "to tidy up (perf.)", spanish: "uklidit (perf.)", german: "aufräumen (perf.)" },
-  { czech: "uklízet (imperf.)", english: "to tidy up (imperf.)", spanish: "uklidit (imperf.)", german: "aufräumen (imperf.)" },
-
-  { czech: "uvařit (perf.)", english: "to cook (perf.)", spanish: "uvařit (perf.)", german: "kochen (perf.)" },
-  { czech: "vařit (imperf.)", english: "to cook (imperf.)", spanish: "vařit (imperf.)", german: "kochen (imperf.)" },
-
-  { czech: "zavolat (perf.)", english: "to call (perf.)", spanish: "zavolat (perf.)", german: "anrufen (perf.)" },
-  { czech: "volat (imperf.)", english: "to call (imperf.)", spanish: "volat (imperf.)", german: "rufen / anrufen (imperf.)" },
-
-  { czech: "ztratit (perf.)", english: "to lose (perf.)", spanish: "ztratit (perf.)", german: "verlieren (perf.)" },
-  { czech: "ztrácet (imperf.)", english: "to lose (imperf.)", spanish: "ztrácet (imperf.)", german: "verlieren (imperf.)" },
-
-  { czech: "vyžehlit (perf.)", english: "to iron (perf.)", spanish: "vyžehlit (perf.)", german: "bügeln (perf.)" },
-  { czech: "žehlit (imperf.)", english: "to iron (imperf.)", spanish: "žehlit (imperf.)", german: "bügeln (imperf.)" }
+  {
+    czech:   "přečíst",
+    english: "to read (perfective)",
+    spanish: "leer (perfective)",
+    german:  "lesen (einmalig)"
+  },
+  {
+    czech:   "číst",
+    english: "to be reading (imperfective)",
+    spanish: "estar leyendo (imperfective)",
+    german:  "lesen (wiederholend)"
+  },
+  {
+    czech:   "udělat",
+    english: "to do (perfective)",
+    spanish: "hacer (perfective)",
+    german:  "machen (einmalig)"
+  },
+  {
+    czech:   "dělat",
+    english: "to be doing (imperfective)",
+    spanish: "estar haciendo (imperfective)",
+    german:  "machen (wiederholend)"
+  },
+  {
+    czech:   "dát",
+    english: "to give (perfective)",
+    spanish: "dar (perfective)",
+    german:  "geben (einmalig)"
+  },
+  {
+    czech:   "dávat",
+    english: "to be giving (imperfective)",
+    spanish: "estar dando (imperfective)",
+    german:  "geben (wiederholend)"
+  },
+  {
+    czech:   "podívat se",
+    english: "to look (perfective)",
+    spanish: "mirar (perfective)",
+    german:  "ansehen (einmalig)"
+  },
+  {
+    czech:   "dívat se",
+    english: "to be looking (imperfective)",
+    spanish: "estar mirando (imperfective)",
+    german:  "sehen (wiederholend)"
+  },
+  {
+    czech:   "dostat",
+    english: "to get (perfective)",
+    spanish: "recibir (perfective)",
+    german:  "bekommen (einmalig)"
+  },
+  {
+    czech:   "dostávat",
+    english: "to be getting (imperfective)",
+    spanish: "estar recibiendo (imperfective)",
+    german:  "bekommen (wiederholend)"
+  },
+  {
+    czech:   "sníst",
+    english: "to eat up (perfective)",
+    spanish: "comer (perfective)",
+    german:  "aufessen (einmalig)"
+  },
+  {
+    czech:   "jíst",
+    english: "to be eating (imperfective)",
+    spanish: "estar comiendo (imperfective)",
+    german:  "essen (wiederholend)"
+  },
+  {
+    czech:   "koupit",
+    english: "to buy (perfective)",
+    spanish: "comprar (perfective)",
+    german:  "kaufen (einmalig)"
+  },
+  {
+    czech:   "kupovat",
+    english: "to be buying (imperfective)",
+    spanish: "estar comprando (imperfective)",
+    german:  "kaufen (wiederholend)"
+  },
+  {
+    czech:   "vyluxovat",
+    english: "to vacuum (perfective)",
+    spanish: "aspirar (perfective)",
+    german:  "staubsaugen (einmalig)"
+  },
+  {
+    czech:   "luxovat",
+    english: "to be vacuuming (imperfective)",
+    spanish: "estar aspirando (imperfective)",
+    german:  "staubsaugen (wiederholend)"
+  },
+  {
+    czech:   "namalovat obraz",
+    english: "to paint a picture (perfective)",
+    spanish: "pintar un cuadro (perfective)",
+    german:  "ein Bild malen (einmalig)"
+  },
+  {
+    czech:   "malovat obraz",
+    english: "to be painting a picture (imperfective)",
+    spanish: "estar pintando un cuadro (imperfective)",
+    german:  "ein Bild malen (wiederholend)"
+  },
+  {
+    czech:   "umýt",
+    english: "to wash (perfective)",
+    spanish: "lavar (perfective)",
+    german:  "waschen (einmalig)"
+  },
+  {
+    czech:   "mýt",
+    english: "to be washing (imperfective)",
+    spanish: "estar lavando (imperfective)",
+    german:  "waschen (wiederholend)"
+  },
+  {
+    czech:   "opravit",
+    english: "to repair (perfective)",
+    spanish: "reparar (perfective)",
+    german:  "reparieren (einmalig)"
+  },
+  {
+    czech:   "opravovat",
+    english: "to be repairing (imperfective)",
+    spanish: "estar reparando (imperfective)",
+    german:  "reparieren (wiederholend)"
+  },
+  {
+    czech:   "vyprat",
+    english: "to wash clothes (perfective)",
+    spanish: "lavar ropa (perfective)",
+    german:  "waschen (einmalig)"
+  },
+  {
+    czech:   "prát",
+    english: "to be washing clothes (imperfective)",
+    spanish: "estar lavando ropa (imperfective)",
+    german:  "waschen (wiederholend)"
+  },
+  {
+    czech:   "vypít",
+    english: "to drink up (perfective)",
+    spanish: "beber (perfective)",
+    german:  "austrinken (einmalig)"
+  },
+  {
+    czech:   "pít",
+    english: "to be drinking (imperfective)",
+    spanish: "estar bebiendo (imperfective)",
+    german:  "trinken (wiederholend)"
+  },
+  {
+    czech:   "zaplatit",
+    english: "to pay (perfective)",
+    spanish: "pagar (perfective)",
+    german:  "bezahlen (einmalig)"
+  },
+  {
+    czech:   "platit",
+    english: "to be paying (imperfective)",
+    spanish: "estar pagando (imperfective)",
+    german:  "bezahlen (wiederholend)"
+  },
+  {
+    czech:   "prodat",
+    english: "to sell (perfective)",
+    spanish: "vender (perfective)",
+    german:  "verkaufen (einmalig)"
+  },
+  {
+    czech:   "prodávat",
+    english: "to be selling (imperfective)",
+    spanish: "estar vendiendo (imperfective)",
+    german:  "verkaufen (wiederholend)"
+  },
+  {
+    czech:   "připravit",
+    english: "to prepare (perfective)",
+    spanish: "preparar (perfective)",
+    german:  "vorbereiten (einmalig)"
+  },
+  {
+    czech:   "připravovat",
+    english: "to be preparing (imperfective)",
+    spanish: "estar preparando (imperfective)",
+    german:  "vorbereiten (wiederholend)"
+  },
+  {
+    czech:   "napsat",
+    english: "to write (perfective)",
+    spanish: "escribir (perfective)",
+    german:  "schreiben (einmalig)"
+  },
+  {
+    czech:   "psát",
+    english: "to be writing (imperfective)",
+    spanish: "estar escribiendo (imperfective)",
+    german:  "schreiben (wiederholend)"
+  },
+  {
+    czech:   "zatelefonovat",
+    english: "to make a call (perfective)",
+    spanish: "llamar (perfective)",
+    german:  "anrufen (einmalig)"
+  },
+  {
+    czech:   "telefonovat",
+    english: "to be making a call (imperfective)",
+    spanish: "estar llamando (imperfective)",
+    german:  "telefonieren (wiederholend)"
+  },
+  {
+    czech:   "vyžehlit",
+    english: "to iron (perfective)",
+    spanish: "planchar (perfective)",
+    german:  "bügeln (einmalig)"
+  },
+  {
+    czech:   "žehlit",
+    english: "to be ironing (imperfective)",
+    spanish: "estar planchando (imperfective)",
+    german:  "bügeln (wiederholend)"
+  },
+  {
+    czech:   "zavolat",
+    english: "to call (perfective)",
+    spanish: "llamar (perfective)",
+    german:  "anrufen (einmalig)"
+  },
+  {
+    czech:   "volat",
+    english: "to be calling (imperfective)",
+    spanish: "estar llamando (imperfective)",
+    german:  "rufen / anrufen (wiederholend)"
+  },
+  {
+    czech:   "ztratit",
+    english: "to lose (perfective)",
+    spanish: "perder (perfective)",
+    german:  "verlieren (einmalig)"
+  },
+  {
+    czech:   "ztrácet",
+    english: "to be losing (imperfective)",
+    spanish: "estar perdiendo (imperfective)",
+    german:  "verlieren (wiederholend)"
+  }
 ];
